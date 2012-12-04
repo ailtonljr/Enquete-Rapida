6329eac8c1eac26b0f9eb40038fc612084edf2a5
Ailton Luiz Dias Siqueira Junior <ailtonljr@gmail.com>
2012-12-04 14:27:28 +0000

    Intial quiz commit.

--- server.js
+++ server.js
@@ -17,11 +17,25 @@ app.configure(function () {
 app.post('/message', function(req, res) {
   fayeClient.publish('/chatroom', {
     userId: req.body.userId,
-    message: req.body.message
+    message: req.body.message,
+    pergunta: req.body.pergunta,
+    alt1: req.body.alt1,
+    alt2:req.body.alt2,
+    alt3: req.body.alt3,
+    alt4: req.body.alt4
   });
 
   res.send(200);
 });
+
+app.post('/resposta', function(req, res) {
+  fayeClient.publish('/resposta', {
+    resp: req.body.resposta
+  });
+
+  res.send(200);
+});
+
 
 fayeServer.listen(8001);
 app.listen(8000);
